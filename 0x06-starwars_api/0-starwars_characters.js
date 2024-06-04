9ee89b8b20d5c4be4129b87ff7d8f5505712a3d0
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-04 07:37:36 +0100

    star wars ✅

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,31 @@
 #!/usr/bin/node
 
-const request = require('request')
+const request = require('request');
 
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
-request.get(url, (err, resp, body) =>{
-    if (err) {
-        console.error(err);
-    } else {
-        const data = JSON.parse(body);
-        const characters = data.characters;
+request.get(url, (err, resp, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    const charNames = [];
+    let count = 0;
 
-        const charNames = []
-        
-        for (let i = 0 ; i < characters.length ; i++) {
-            request.get(characters[i], (err, resp, body) =>{
-                if (err) {
-                    console.log(err)
-                } else {
-                    let data_name = JSON.parse(body);
-                    console.log(data_name.name)
-                    charNames.push(data_name.name);
-                }
-            });
+    for (let i = 0; i < characters.length; i++) {
+      request.get(characters[i], (err, resp, body) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const dataName = JSON.parse(body);
+          charNames[i] = dataName.name;
+          count++;
+          if (count === characters.length) {
+            charNames.forEach(name => console.log(name));
+          }
         }
+      });
     }
+  }
 });
