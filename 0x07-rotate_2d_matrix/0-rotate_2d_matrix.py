a0c9db8e49dbe4248b82bc6c65a7f39e55f3d04e
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-13 13:51:31 +0100

    2d matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,18 +3,17 @@
 
 
 def rotate_2d_matrix(matrix):
+    """ rotate 2d matrix """
     n = len(matrix)
-    
-    # Transpose the matrix
+
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
-    # Reverse each row
+
     for i in range(n):
         matrix[i].reverse()
 
-# Test the function
+
 if __name__ == "__main__":
     matrix = [
         [1, 2, 3],
