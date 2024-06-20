bfbfc95213078d6f7c81c7e5e1a58fac44a19895
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-06-20 18:43:25 +0100

    making changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" making-changes """
+
+
+def makeChange(coins, total):
+    if total < 0:
+        return 0
+    if total == 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
